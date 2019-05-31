19f4cebe23a834760eeac2d6dcdeb5904e28ccfb
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-05-31 19:13:53 -0400

    add components.lua

---
+++ Sunshine/members/systems/14_uiRenderSystem.lua
@@ -0,0 +1,51 @@
+-- TrafficConeGod
+
+return function(Sunshine, entity)
+    local model = entity.model
+    local transform = entity.transform
+    local physics = entity.physics
+    if model and transform then
+        model.model = model.model:Clone()
+        Sunshine:addInstance(model.model)
+        model.model.Name = entity.core.id
+        model.model:SetPrimaryPartCFrame(transform.cFrame)
+        model.model.Parent = workspace
+        local lastSize = Vector3.new(1, 1, 1)
+        local originalCFrames = {}
+        for index, descendant in pairs(model.model:GetDescendants()) do
+            if descendant:IsA("BasePart") then
+                originalCFrames[descendant] = descendant.CFrame
+            end
+        end
+        transform.cFrame = nil
+        setmetatable(transform, {
+            __index = function(self, key)
+                if not model.model.Parent then
+                    setmetatable(transform, {})
+                    return CFrame.new()
+                end
+                if key == "cFrame" then
+                    return model.model:GetPrimaryPartCFrame()
+                end
+            end,
+            __newindex = function(self, key, value)
+                if not model.model.Parent then
+                    setmetatable(transform, {})
+                    return
+                end
+                if key == "cFrame" then
+                    model.model:SetPrimaryPartCFrame(value)
+                end
+            end
+        })
+        Sunshine:update(function()
+            for index, descendant in pairs(model.model:GetDescendants()) do
+                if descendant:IsA("BasePart") then
+                    descendant.Size = descendant.Size / lastSize
+                    descendant.Size = descendant.Size * transform.size
+                end
+            end
+            lastSize = transform.size
+        end)
+    end
+end
