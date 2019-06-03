c158c4df39929f0db3f4223d6c3da8fc44d08029
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-06-03 15:21:48 -0400

    add an actually fun dive

--- Sunshine/members/unloadScene.lua
+++ Sunshine/members/unloadScene.lua
@@ -7,14 +7,6 @@ return function(Sunshine)
         for _, instance in pairs(Sunshine.instances) do
             instance:Destroy()
         end
-        local sky = game.ReplicatedStorage.Assets.skies.BrightBlue:Clone()
-        sky.Parent = game.Lighting
-        Sunshine:addInstance(sky)
-		game.Lighting.Brightness = 2
-		game.Lighting.GlobalShadows = true
-		game.Lighting.ClockTime = 12
-		game.Lighting.FogStart = 0
-		game.Lighting.FogEnd = 100000
         Sunshine.dataScene = nil
         Sunshine.scene = nil
     end
