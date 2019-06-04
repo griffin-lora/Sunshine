b4db74a76c6df182afcdfc692ed2925e2b79844a
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-06-04 10:45:02 -0400

    add Sunshine:createStateSystem

--- Sunshine/members/systems/10_diveSystem.lua
+++ Sunshine/members/systems/10_diveSystem.lua
@@ -9,30 +9,24 @@ return function(Sunshine, entity)
     local transform = entity.transform
     local physics = entity.physics
     local animator = entity.animator
-    if component and character and input and transform and physics and animator then
-        local lastState = character.state
-        local horizontal
-        Sunshine:update(function(step)
-            if character.controllable then
-                if character.state == state then
-                    -- update
-                    horizontal = horizontal:Lerp(transform.cFrame.LookVector * component.power, step * 3)
-                    physics.velocity = Vector3.new(horizontal.X, physics.velocity.Y, horizontal.Z)
-                    if character.grounded or (lastState == state and character.state ~= state) then
-                        -- end
-                        character.state = nil
-                        animator.action = 2794459258
-                    end
-                elseif character.state == "groundPound" and not character.grounded and input.e or (lastState ~= state and character.state == state) then
-                    -- start
-                    character.state = state
-                    physics.movable = true
-                    horizontal = transform.cFrame.LookVector * component.power
-                    physics.velocity = horizontal + Vector3.new(0, 30, 0)
-                    animator.action = 1146922909
-                end
-            end
-            lastState = character.state
-        end)
-    end
+    Sunshine:createStateSystem(entity, state, function()
+        -- start check
+        return character.state == "groundPound" and not character.grounded and input.e
+    end, function()
+        -- start
+        physics.movable = true
+        horizontal = transform.cFrame.LookVector * component.power
+        physics.velocity = horizontal + Vector3.new(0, 30, 0)
+        animator.action = 1146922909
+    end, function(step)
+        -- update
+        horizontal = horizontal:Lerp(transform.cFrame.LookVector * component.power, step * 3)
+        physics.velocity = Vector3.new(horizontal.X, physics.velocity.Y, horizontal.Z)
+    end, function()
+        -- end check
+        return character.grounded
+    end, function()
+        -- end
+        animator.action = 2794459258
+    end)
 end
