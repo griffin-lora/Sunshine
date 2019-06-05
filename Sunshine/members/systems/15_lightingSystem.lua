bc4ac78f6cb80c0a41872adc08789df0c031421b
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-06-05 10:48:37 -0400

    SuperMakerPlayer actually did this

--- Sunshine/members/systems/15_lightingSystem.lua
+++ Sunshine/members/systems/15_lightingSystem.lua
@@ -10,11 +10,31 @@ return function(Sunshine, entity)
 		Sunshine:addInstance(sky)
 		Lighting.Ambient = lighting.ambient
 		Lighting.OutdoorAmbient = lighting.outdoorAmbient
-		Lighting.Brightness = lighting.brightness
+		Lighting.Brightness = lighting.lightness
 		Lighting.GlobalShadows = lighting.globalShadows
 		Lighting.ClockTime = lighting.clockTime
+		Lighting.GeographicLatitude = lighting.sunPos
 		Lighting.FogColor = lighting.fogColor
 		Lighting.FogStart = lighting.fogStart
 		Lighting.FogEnd = lighting.fogEnd
+		local color = Instance.new("ColorCorrectionEffect")
+		Sunshine:addInstance(color)
+		local blur = Instance.new("BlurEffect")
+		Sunshine:addInstance(blur)
+		local bloom = Instance.new("BloomEffect")
+		Sunshine:addInstance(bloom)
+		local rays = Instance.new("SunRaysEffect")
+		Sunshine:addInstance(rays)
+		color.Brightness = lighting.brightness
+		color.Contrast = lighting.contrast
+		color.Saturation = lighting.saturation
+		color.TintColor = lighting.tintColor
+		blur.Size = lighting.blurAmount
+		bloom.Threshold = lighting.bloomThreshold
+		rays.Enabled = lighting.sunRays
+		color.Parent = game.Lighting
+		blur.Parent = game.Lighting
+		bloom.Parent = game.Lighting
+		rays.Parent = game.Lighting
 	end
 end
