1ac5aaaf763132464434edf5acf72c66fc9a9146
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-06-08 20:26:37 -0700

    fix some more of the moving platforms
    how the fuck am i supposed to get velocity to work

--- Sunshine/members/systems/23_platformSystem.lua
+++ Sunshine/members/systems/23_platformSystem.lua
@@ -1,24 +1,32 @@
-local moveTo = "destination"
 
 return function(Sunshine, entity)
     local transform = entity.transform
     local moving = entity.moving
+    local physics = entity.physics
+    local moveTo = "destination"
 
     if transform and moving then
+        local lastLerp = transform.cFrame
         if moving.speed ~= 0 then
             local start = transform.cFrame
             local destination = transform.cFrame * moving.extend
             Sunshine:update(function(step)
-                if transform.cFrame == start and moveTo == "start" then
+                if (transform.cFrame.Position - start.Position).Magnitude < 1 and moveTo == "start" then
                     moveTo = "destination"
-                elseif transform.cFrame == destination and moveTo == "destination" then
+                elseif (transform.cFrame.Position - destination.Position).Magnitude < 1  and moveTo == "destination" then
                     moveTo = "start"
                 end
                 
                 if moveTo == "destination" then
-                    transform.cFrame = transform.cFrame:Lerp(destination, step * moving.speed)
+                    local lerp = transform.cFrame:Lerp(destination, step * moving.speed)
+                    transform.cFrame = lerp
+                    physics.velocity = physics.velocity + (lerp.Position - lastLerp.Position)
+                    lastLerp = transform.cFrame
                 elseif moveTo == "start" then
-                    transform.cFrame = transform.cFrame:Lerp(start, step * moving.speed)
+                    local lerp = transform.cFrame:Lerp(start, step * moving.speed)
+                    transform.cFrame = lerp
+                    physics.velocity = physics.velocity + (lerp.Position - lastLerp.Position)
+                    lastLerp = transform.cFrame
                 end
             end)
         end
