2e02d143fbe4f2b7934bf417a65f5749de8d6b04
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-06-25 13:48:09 -0400

    remove unused systems

--- unusedSystems/10_spinnerSystem.lua
+++
@@ -1,9 +0,0 @@
-return function(Sunshine, entity)
-    local transform = entity.transform
-    local spinner = entity.spinner
-    if transform and spinner then
-        Sunshine:update(function()
-            transform.cFrame = CFrame.new(transform.cFrame.Position) * CFrame.Angles(0, (tick() % math.pi) * spinner.speed, 0)
-        end)
-    end
-end
