e5eddeb3f59fa9da6590a44375f15864b37151bc
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-07-17 15:09:56 -0400

    remove unecessary statement

--- Sunshine/members/unloadScene.lua
+++ Sunshine/members/unloadScene.lua
@@ -1,9 +1,7 @@
 return function(Sunshine, scene)
     if scene then
         for _, entity in pairs(scene.entities) do
-            if Sunshine.entityScenes[entity] == scene then
-                Sunshine:destroyEntity(entity)
-            end
+            Sunshine:destroyEntity(entity)
         end
     end
 end
