ca840cea6a4e8f6eb9d6eb14fbbb8ee3e63df2f9
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-07-26 17:13:50 -0700

    hide topbar

--- Client/init.client.lua
+++ Client/init.client.lua
@@ -1,5 +1,10 @@
 local Sunshine = require(game:GetService("ReplicatedStorage"):WaitForChild("Sunshine"))
 local Assets = game:GetService("ReplicatedStorage"):WaitForChild("Assets")
 
+pcall(function()
+	local starterGui = game:GetService('StarterGui')
+	starterGui:SetCore("TopbarEnabled", false)
+end)
+
 Sunshine:addSystemFolder(script:WaitForChild("systems"))
 Sunshine:loadScene(require(Assets.scenes.CrossroadsDay), 1)
