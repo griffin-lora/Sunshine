033deb8ddc7224de7c3200574cc6eaa058ce5553
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-06 08:54:53 -0700

    implement speed

--- Client/systems/65_swingingPlatformSystem.lua
+++ Client/systems/65_swingingPlatformSystem.lua
@@ -12,7 +12,7 @@ return function(Sunshine, entity)
             transform.cFrame = CFrame.new(transform.cFrame.Position) * CFrame.Angles(math.rad(rotation), oldRotation.Y,
             oldRotation.Z)
             ----------------------------
-            rotation = math.sin(entity.core.tick) * sup.range
+            rotation = math.sin(entity.core.tick * sup.speed) * sup.range
             ----------------------------
             for _, descendant in pairs(model.model:GetDescendants()) do
                 if CollectionService:HasTag(descendant, "uprightPlatform") then
