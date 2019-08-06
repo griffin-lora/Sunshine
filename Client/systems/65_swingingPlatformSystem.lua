3bc84e0ad14dc8f9204c178cb3918a5f5fd9f22b
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-06 08:52:15 -0700

    please luacheck
    maker please maker your code better ok

--- Client/systems/65_swingingPlatformSystem.lua
+++ Client/systems/65_swingingPlatformSystem.lua
@@ -5,19 +5,19 @@ return function(Sunshine, entity)
     local model = entity.model
     local transform = entity.transform
     local rotation = 0
-    local reverse = false
 
     if sup and model then
         local oldRotation = transform.cFrame - transform.cFrame.Position
         Sunshine:update(function()
-            transform.cFrame = CFrame.new(transform.cFrame.Position) * CFrame.Angles(math.rad(rotation),oldRotation.Y,oldRotation.Z)
+            transform.cFrame = CFrame.new(transform.cFrame.Position) * CFrame.Angles(math.rad(rotation), oldRotation.Y,
+            oldRotation.Z)
             ----------------------------
             rotation = math.sin(entity.core.tick) * sup.range
             ----------------------------
-            for _,p in pairs(model.model:GetDescendants()) do
-                if CollectionService:HasTag(p, "uprightPlatform") then
-                    local x, y, z = p.CFrame:toEulerAnglesXYZ()
-                    p.CFrame = CFrame.new(p.Position) * CFrame.Angles(0,y,z)
+            for _, descendant in pairs(model.model:GetDescendants()) do
+                if CollectionService:HasTag(descendant, "uprightPlatform") then
+                    local _, y, z = descendant.CFrame:toEulerAnglesXYZ()
+                    descendant.CFrame = CFrame.new(descendant.Position) * CFrame.Angles(0, y, z)
                 end
             end
         end, entity)
