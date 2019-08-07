d81b9420fc690d631e48407d32d58dd1e68b7ae4
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-08-07 16:35:04 -0700

    add health tweening

--- Client/systems/63_healthUISystem.lua
+++ Client/systems/63_healthUISystem.lua
@@ -1,8 +1,15 @@
 local CollectionService = game:GetService("CollectionService")
+local TWEENINFO_BACK = TweenInfo.new(.5, Enum.EasingStyle.Sine, Enum.EasingDirection.Out, 0, false, 0)
+local CHARACTER_POSITION_OFFSET = 70
 
 return function(Sunshine, entity)
     local healthUI = entity.healthUI
     local frame = entity.frame
+    local lastHealth
+    local startTick
+    local startTickBack
+    local firstPosition
+    local screenPointPosition
 
     if healthUI and frame then
         Sunshine:update(function()
@@ -12,9 +19,15 @@ return function(Sunshine, entity)
                     local mainCharacter = Sunshine:getEntity(player.player.mainCharacter, Sunshine.scenes[1])
                     if mainCharacter then
                         local health = mainCharacter.health
+                        if not lastHealth then
+                            lastHealth = health.health
+                        end
                         if health then
                             for _,p in pairs(frame.frame:GetDescendants()) do
                                 if CollectionService:HasTag(p, "healthLabel") then
+                                    if not firstPosition then
+                                        firstPosition = p.Position
+                                    end
                                     p.Image = "rbxassetid://"..healthUI.image
                                     if health.health >= 3 then
                                         p.ImageRectOffset = healthUI.rectOffset3
@@ -25,8 +38,25 @@ return function(Sunshine, entity)
                                     else
                                         p.ImageRectOffset = healthUI.rectOffset0
                                     end
+                                    if health.health ~= lastHealth and health.health ~= 0 and (health.health ~= 3 and lastHealth ~= 0) then
+                                        local screenPoint = workspace.Camera:WorldToScreenPoint(mainCharacter.transform.cFrame.Position)
+                                        p.Position = UDim2.new(0, screenPoint.x, 0, screenPoint.y - CHARACTER_POSITION_OFFSET)
+                                        screenPointPosition = p.Position
+                                        startTick = entity.core.tick
+                                        startTickBack = nil
+                                    end
+                                    if startTick and entity.core.tick - startTick > 2 then
+                                        startTick = nil
+                                        startTickBack = entity.core.tick 
+                                    end
+                                    if startTickBack and entity.core.tick - startTickBack < .5 then
+                                        p.Position = UDim2.new(Sunshine:tween(entity.core.tick - startTickBack, TWEENINFO_BACK, screenPointPosition.X.Scale, firstPosition.X.Scale), Sunshine:tween(entity.core.tick - startTickBack, TWEENINFO_BACK, screenPointPosition.X.Offset, firstPosition.X.Offset), Sunshine:tween(entity.core.tick - startTickBack, TWEENINFO_BACK, screenPointPosition.Y.Scale, firstPosition.Y.Scale), Sunshine:tween(entity.core.tick - startTickBack, TWEENINFO_BACK, screenPointPosition.Y.Offset, firstPosition.Y.Offset) )
+                                    elseif startTickBack then
+                                        startTickBack = nil
+                                    end
                                 end
                             end
+                            lastHealth = health.health
                         end
                     end
                 end
