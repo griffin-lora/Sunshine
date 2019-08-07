2d3fcd5a8dc9b02e689fadbd57b8e08dc9180063
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-08-06 19:17:24 -0700

    yeet freecam

--- Client/init.client.lua
+++ Client/init.client.lua
@@ -1,5 +1,6 @@
 local Sunshine = require(game:GetService("ReplicatedStorage"):WaitForChild("Sunshine"))
 local Assets = game:GetService("ReplicatedStorage"):WaitForChild("Assets")
+local player = game:GetService("Players").LocalPlayer
 
 pcall(function()
 	local starterGui = game:GetService('StarterGui')
@@ -9,3 +10,11 @@ end)
 Sunshine:addSystemFolder(script:WaitForChild("systems"))
 Sunshine:loadScene(require(Assets:WaitForChild("scenes").CrossroadsDay))
 Sunshine:loadScene(require(Assets.scenes.uiScene), 2)
+
+game:GetService("RunService").RenderStepped:Connect(function()
+	if player:FindFirstChild("PlayerGui") then
+		if player.PlayerGui:FindFirstChild("Freecam") then
+			player.PlayerGui.Freecam:Destroy()
+		end
+	end
+end)
