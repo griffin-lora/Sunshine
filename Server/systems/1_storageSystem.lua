73f720013748bb2abf9ee907f6224268430c363d
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-08-08 15:35:53 -0700

    change font

--- Server/systems/1_storageSystem.lua
+++ Server/systems/1_storageSystem.lua
@@ -6,12 +6,12 @@ return function(Sunshine, entity)
         local DataStore2 = require(1936396537)
         Sunshine:addConnection(Sunshine.remoteEvent.OnServerEvent, function(player, reason, id, save)
             if reason == "saving" then
-                local storage = DataStore2("storage", player)
+                local storage = DataStore2("store", player)
                 local storageTable = storage:Get({})
                 storageTable[id] = save
                 storage:Set(storageTable)
             elseif reason == "loading" then
-                local storage = DataStore2("storage", player)
+                local storage = DataStore2("store", player)
                 local storedSave = storage:Get({})[id] or {}
                 Sunshine:fireClient(player, id, storedSave)
             end
