4e2f35bc4fdc405e3c266b47638ff67ad04699b6
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-08-08 11:45:21 -0700

    add regional coins to coin counter

--- Client/systems/32_coinCounterSystem.lua
+++ Client/systems/32_coinCounterSystem.lua
@@ -5,6 +5,7 @@ return function(Sunshine, entity)
     local coinCounter = entity.coinCounter
     local frame = entity.frame
     local last = 0
+    local lastReg = 0
     if coinCounter and frame then
         Sunshine:update(function()
             if Sunshine.scenes[1] then
@@ -20,6 +21,18 @@ return function(Sunshine, entity)
                             end
                             for _,p in pairs(frame.frame:GetDescendants()) do
                                 if CollectionService:HasTag(p, "coinsLabel") then
+                                    p.Text = text
+                                end
+                            end
+                        end
+                        if lastReg ~= stats.regionalCoins then
+                            lastReg = stats.regionalCoins
+                            local text = stats.regionalCoins
+                            for _ = 1, 3 - #tostring(text) do
+                                text = "0"..text
+                            end
+                            for _,p in pairs(frame.frame:GetDescendants()) do
+                                if CollectionService:HasTag(p, "regionalCoinsLabel") then
                                     p.Text = text
                                 end
                             end
