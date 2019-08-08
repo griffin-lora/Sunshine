91d8abf4fbd3b109cdabdb0c9530155f84ae90f2
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-08 08:00:48 -0700

    maker fuck you

--- Server/scene.lua
+++ Server/scene.lua
@@ -1,1 +1,10 @@
-return {entities = {}}
+return {entities = {
+    {
+        core = {
+            id = "{660B19FE-D549-40CF-8EDE-561413AB603A}",
+            name = "DataStore",
+            active = true
+        },
+        store = {}
+    }
+}}
