2af901bc5da5294470d02afbd9b055c0479cc427
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-08 14:47:25 -0700

    add suite saving
    very broken

--- Suite/init.server.lua
+++ Suite/init.server.lua
@@ -1,12 +1,20 @@
-if not game:GetService("ReplicatedStorage"):FindFirstChild("Sunshine") then
-    error("Sunshine not found. Aborting.")
+local Sunshine = require(game:GetService("ReplicatedStorage"):WaitForChild("Sunshine"))
+
+for _, child in pairs(workspace:GetChildren()) do
+    if child:IsA("Message") and child.Name == "SunshineSuiteWorkspace" then
+        child:Destroy()
+    end
 end
-
-local Sunshine = require(game:GetService("ReplicatedStorage").Sunshine)
-
 Sunshine.workspace = Instance.new("Message")
+Sunshine.workspace.Name = "SunshineSuiteWorkspace"
 Sunshine.workspace.Parent = workspace
+Sunshine.gui = Instance.new("ScreenGui")
+Sunshine.gui.Name = "SunshineSuiteScreenGui"
+Sunshine.gui.IgnoreGuiInset = true -- why does this property need to exist it should always be true
+Sunshine.gui.ResetOnSpawn = false -- again should always be false
+Sunshine.gui.Parent = game:GetService("CoreGui") -- ANGERY
 Sunshine.plugin = plugin
+
 Sunshine:manage()
 Sunshine:addSystemFolder(script:WaitForChild("systems"))
 Sunshine:loadScene(require(script:WaitForChild("scene")))
