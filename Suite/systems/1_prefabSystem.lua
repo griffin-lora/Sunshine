4e01604e00fb0416c9fba3887f065d361acbd80e
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-10 10:06:03 -0700

    REVERT

--- Suite/systems/1_prefabSystem.lua
+++ Suite/systems/1_prefabSystem.lua
@@ -3,15 +3,15 @@
 return function(Sunshine, entity)
     local prefab = entity.prefab
     if prefab then
-        -- local dataPrefab = require(prefab.prefab)
-        -- for name, component in pairs(dataPrefab) do
-        --     if not entity[name] then
-        --         entity[name] = Sunshine:cloneTable(component)
-        --     end
-        -- end
-        -- if dataPrefab.core then
-        --     entity.core.id = dataPrefab.core.id
-        -- end
-        -- entity.core.active = true
+        local dataPrefab = require(prefab.prefab)
+        for name, component in pairs(dataPrefab) do
+            if not entity[name] then
+                entity[name] = Sunshine:cloneTable(component)
+            end
+        end
+        if dataPrefab.core then
+            entity.core.id = dataPrefab.core.id
+        end
+        entity.core.active = true
     end
 end
