b383eff7c1815ce03590e046f2dac318bcf4e4c4
TrafficConeGod <36275551+TrafficConeGod@users.noreply.github.com>
2019-08-12 19:36:41 -0700

    add attempt at some team create stuff

--- PluginNetworkClient/init.lua
+++ PluginNetworkClient/init.lua
@@ -1,4 +1,29 @@
 local PluginNetworkClient = {}
+local clientEvents = {}
+local player = game:GetService("Players").LocalPlayer
+if not player:FindFirstChild("usingPNC") then
+    local usingPNC = Instance.new("BoolValue")
+    usingPNC.Name = "usingPNC"
+    usingPNC.Parent = player
+end
+local packetFolder = game:GetService("ServerStorage"):FindFirstChild("PNCPacketFolder") or Instance.new("Folder")
+packetFolder.Name = "PNCPacketFolder"
+packetFolder.Parent = game:GetService("ServerStorage")
+packetFolder.ChildAdded:Connect(function(packet)
+    if packet.from.Value ~= player then
+        if packet:FindFirstChild("to") then
+            if packet.to.Value == player then
+                for _, event in pairs(clientEvents) do
+                    event(packet.from.Value, unpack(require(packet)))
+                end
+            end
+        else
+            for _, event in pairs(clientEvents) do
+                event(packet.from.Value, unpack(require(packet)))
+            end
+        end
+    end
+end)
 
 function PluginNetworkClient.createPacket(_, ...)
     local packet = Instance.new("ModuleScript")
@@ -18,22 +43,48 @@ function PluginNetworkClient.createPacket(_, ...)
     end
     source = source .. "}"
     packet.Source = source
+    spawn(function()
+        wait(10)
+        packet:Destroy()
+    end)
     return packet
 end
 
 function PluginNetworkClient.fireClient(_, client, ...)
     local packet = PluginNetworkClient:createPacket(...)
-    print(packet.Source)
+    packet.Name = "Client"
+    local from = Instance.new("ObjectValue")
+    from.Name = "from"
+    from.Value = player
+    from.Parent = packet
+    local to = Instance.new("ObjectValue")
+    to.Name = "to"
+    to.Value = client
+    to.Parent = packet
+    packet.Parent = packetFolder
 end
 
 
 function PluginNetworkClient.fireAllClients(_, ...)
     local packet = PluginNetworkClient:createPacket(...)
-    print(packet.Source)
+    local from = Instance.new("ObjectValue")
+    from.Name = "from"
+    from.Value = player
+    from.Parent = packet
+    local received = Instance.new("Folder")
+    received.Name = "received"
+    received.Parent = packet
+    packet.Parent = packetFolder
 end
 
 function PluginNetworkClient.onClientEvent(_, callback)
-    -- callback(client, ...)
+    local index = #clientEvents + 1
+    clientEvents[index] = callback
+    return index
+end
+
+function PluginNetworkClient.removeClientEvent(_, index)
+    table.remove(clientEvents, index)
 end
 
 return PluginNetworkClient
--- Suite/systems/10_changeSystem.lua
+++ Suite/systems/10_changeSystem.lua
@@ -12,7 +12,8 @@ return function(Sunshine, entity)
                 change.entity.core.dataEntity[change.componentName][change.propertyName] = change.propertyValue
                 change.entity.core.scene.instance.Source = "return " .. Sunshine:encodeTable(Sunshine.dataScenes
                 [change.entity.core.scene.index])
-                Sunshine.PluginNetworkClient:fireAllClients(change.entity.core.scene.instance, change.entity.core.id, change.componentName,
+                Sunshine.PluginNetworkClient:fireAllClients(change.entity.core.scene.instance, change.entity.core.id,
+                change.componentName,
                 change.propertyName, change.propertyValue)
                 change.entity = nil
                 change.componentName = nil
@@ -20,6 +21,12 @@ return function(Sunshine, entity)
                 change.propertyValue = nil
                 change.alreadyChangedOnEntity = nil
             end
+        end, entity)
+        local index = Sunshine.PluginNetworkClient:onClientEvent(function(client, sceneInstance, entityId, componentName, propertyName, propertyValue)
+            print(client,sceneInstance,entityId,componentName,propertyName,propertyValue)
+        end)
+        Sunshine:entityDestroy(function()
+            Sunshine.PluginNetworkClient:removeClientEvent(index)
         end, entity)
     end
 end
