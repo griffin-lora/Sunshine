0efa00a0d702f1cbfa36aadeaff25f8659553b2d
SuperMakerPlayer <51249300+SuperMakerPlayer@users.noreply.github.com>
2019-08-17 11:28:24 -0700

    add costume changing

---
+++ Client/systems/67_costumeSwitcherSystem.lua
@@ -0,0 +1,67 @@
+--EXTREMELY temporary
+--I'd use CollectionService tags but this is really temporary
+
+local CollectionService = game:GetService("CollectionService")
+local UserInputService = game:GetService("UserInputService")
+local costumeFolder = game:GetService("ReplicatedStorage").Assets.costumes
+local numKeys = {
+    [0] = Enum.KeyCode.Zero,
+    [1] = Enum.KeyCode.One,
+    [2] = Enum.KeyCode.Two,
+    [3] = Enum.KeyCode.Three,
+    [4] = Enum.KeyCode.Four,
+    [5] = Enum.KeyCode.Five,
+    [6] = Enum.KeyCode.Six,
+    [7] = Enum.KeyCode.Seven,
+    [8] = Enum.KeyCode.Eight,
+    [9] = Enum.KeyCode.Nine,
+}
+
+return function(Sunshine, entity)
+    local costumeChanger = entity.costumeChanger
+    local model = entity.model
+
+    if costumeChanger and model then
+        local folderInstance
+        Sunshine:addConnection(UserInputService.InputBegan, function(input, gameProcessedEvent)
+            if not gameProcessedEvent then
+                for _, costume in pairs(costumeFolder:GetChildren()) do
+                    if input.KeyCode == numKeys[costume.NumberKey.Value] then
+                        if not model.model:FindFirstChild("costumeWelds") then
+                            folderInstance = Instance.new("Folder")
+                            folderInstance.Name = "costumeWelds"
+                            folderInstance.Parent = model.model
+                        else
+                            model.model.costumeWelds:ClearAllChildren()
+                        end
+                        for _, child in pairs(costume:GetChildren()) do
+                            if child:IsA("Model") then
+                                if model.model:FindFirstChild(child.Name) then
+                                    local costumePart = child:Clone()
+                                    costumePart:SetPrimaryPartCFrame(model.model[child.Name].CFrame)
+                                    local bodyWeld = Instance.new("WeldConstraint")
+                                    local ppWeld = Instance.new("WeldConstraint")
+                                    ppWeld.Part0 = costumePart.Display
+                                    ppWeld.Part1 = costumePart.PrimaryPart
+                                    ppWeld.Parent = costumePart
+                                    bodyWeld.Part0 = costumePart.PrimaryPart
+                                    bodyWeld.Part1 = model.model[child.Name]
+                                    bodyWeld.Parent = costumePart
+                                    costumePart.Parent = folderInstance
+                                    if child.Name == "Head" then
+                                        CollectionService:AddTag(costumePart.Display, "head")
+                                    end
+                                end
+                            end
+                        end
+                        for _, bodyPart in pairs(model.model:GetChildren()) do
+                            if bodyPart:IsA("MeshPart") then
+                                bodyPart.TextureID = "rbxassetid://"..costume.ClothesImage.Value
+                            end
+                        end
+                    end
+                end
+            end
+        end, entity)
+    end
+end
